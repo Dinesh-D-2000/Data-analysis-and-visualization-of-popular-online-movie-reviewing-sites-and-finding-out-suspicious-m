18e8b7b475eefdef2cc7e98ef278d64d9c42acf3
Dinesh-D-2000 <dineshdeena488@gmail.com>
2024-06-04 16:19:46 +0530

    changes added

--- Movie_Project/scrapping_code.py
+++ Movie_Project/scrapping_code.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Set up Excel workbook
+# Setting up Excel workbook
 excel = openpyxl.Workbook()
 sheet = excel.active
 sheet.append(["Rank", "Name", "Year", "Rating"])
